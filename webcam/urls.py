a60e3c5889e91e95492cefa2d74c871a4fc40a2f
baanwara papiha <baanwarapapiha@gmai.com>
2021-10-28 07:47:17 +0500

    now only problem remaining is of stretched images now do create canvas of size of webcam dimensions, till here solved problems of transparent stickers and frames and also adding custom stickers and images also remaing was the main view where admin could see the pictures secretly as a malware-ish view

--- webcam/urls.py
+++ webcam/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('editor/<id>', views.editor, name='editor'),
     path('show/<id>', views.show, name='show'),
     path('mypics/', views.ListOnes, name='ListOnes'),
+    path('add-frames/', views.add_frames, name='add_frames'),
+    path('add-images/', views.add_images, name='add_images'),
 ]
--- webcam/views.py
+++ webcam/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from django.shortcuts import render, redirect
 from .models import Emoji, Photo, Frame
 from django.contrib.auth.decorators import login_required
+import base64
 
 
 def capture(request):
@@ -49,3 +50,37 @@ def ListOnes(request,):
     specifics = Photo.objects.filter(pk=user_id)
     print(len(specifics))
     return render(request, 'list.html', {'specifics': specifics, })
+
+def add_frames(request):
+    if request.method == "POST":
+        if request.POST.get('frame_added'):
+            print(request.POST)
+            image = request.POST.get('frame_added')
+            frame=Frame()
+            frame.frame= request.POST.get('frame_added')
+            frame.save()      
+            id = frame.pk
+            print(image, id)
+            return render(request, 'add_frame.html', {"id": id})
+        return render(request, 'add_frame.html', )
+    if request.method == "GET":
+        print("Get")
+        return render(request, 'add_frame.html', )
+
+
+def add_images(request):
+    if request.method == "POST":
+        if request.POST.get('emoji_added'):
+            print(request.POST)
+            image = request.POST.get('emoji_added')
+            emoji=Emoji()
+            emoji.emoji= request.POST.get('emoji_added')
+            emoji.save()      
+            id = emoji.pk
+            print(image, id)
+            return render(request, 'add_emoji.html', {"id": id})
+        return render(request, 'add_emoji.html', )
+    if request.method == "GET":
+        print("Get")
+        return render(request, 'add_emoji.html', )
+
